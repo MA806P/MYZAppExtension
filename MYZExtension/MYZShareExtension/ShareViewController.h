e348d8ac2afd16132e123405599bfff65bb60169
MA806P <mayp126@126.com>
2017-10-19 18:05:24 +0800

    add share extension target

---
+++ MYZExtension/MYZShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  MYZShareExtension
+//
+//  Created by MA806P on 2017/10/19.
+//  Copyright © 2017年 myz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
