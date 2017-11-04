4efcf22d15c22dd9c388c918cb37aa33e034ad19
MA806P <mayp126@126.com>
2017-11-04 18:06:59 +0800

    update

--- MYZExtension/MYZExtension/MYZTodayDetailViewController.m
+++ MYZExtension/MYZExtension/MYZTodayDetailViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
     self.view.backgroundColor = [UIColor whiteColor];
     
     UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(20, 70, self.view.frame.size.width-40, 100)];
@@ -27,19 +27,7 @@
     [self.view addSubview:label];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
